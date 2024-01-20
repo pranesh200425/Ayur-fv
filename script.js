254b57e21d0b54c09af6e328885da102e90c8c44
pranesh200425 <154605460+pranesh200425@users.noreply.github.com>
2024-01-20 19:55:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+const viewModalBtn = document.querySelectorAll(".view-details");
+const modalClosebtn = document.querySelectorAll(".close-btn");
+const menuIcon = document.querySelector(".menu-icon");
+const modalContainer = document.querySelector(".product-view-modal");
+
+
+
+let isModalvis = false;
+
+ const modOpenClose = () => {
+    let modal = document.querySelector(".product-view-modal");
+    if (isModalvis === false) {
+        modal.classList.add("active");
+        isModalvis = true;
+    } else {
+        modal.classList.remove("active");
+        isModalvis = false;
+    }
+} 
+
+ const openMenu = () => {
+    let bars = document.querySelector(".fa-bars")
+    let menu = document.querySelector(".nav .transparent .nav-items");
+    menu.classList.toggle('show');
+    bars.classList.toggle("fa-xmark")
+    console.log("works");
+} 
+
+  viewModalBtn.forEach(function(button) {
+    button.addEventListener('click', function() {
+        // Get the target element ID from the data-target attribute
+        var targetElementId = button.getAttribute('data-target');
+
+        // Manipulate the corresponding element
+        var targetElement = document.getElementById(targetElementId);
+        if (!isModalvis) {
+            // Add your manipulation logic here
+            targetElement.classList.add("active");
+            isModalvis = true;
+        }
+    });
+});
+
+const closemodal = () => {
+    if(isModalvis){
+        modalContainer.classList.remove("active");
+        isModalvis = false;
+    }
+}
+
+
+modalClosebtn.forEach(function(button) {
+    button.addEventListener('click', function(event) {
+        let parent = event.target.parentNode;
+        let real = parent.parentNode;
+        let prodmod = real.parentNode;
+/* 
+        console.log(parent);
+        console.log(real);
+        console.log(prodmod); */
+        prodmod.classList.remove("active")
+        isModalvis = false;
+    });
+});
+
+    menuIcon.addEventListener("click", openMenu)
+
+ 
